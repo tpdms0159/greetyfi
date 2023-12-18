daa29b4deda39d1642eae2a73dfa97a202f62c1f
tpdms0159 <tpdms0159@naver.com>
2023-12-18 23:53:07 +0900

    add components

---
+++ greetify/src/Pages/components/DivStyles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const Option = styled.div`
+  width: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 3%;
+
+`
+const BoardingView = styled.div`    
+  width: 100%;
+  max-width: 100%;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  maring-bottom: 10%;
+`
+
+
+export { BoardingView };
+export default Option;
